16c5a260feaf456f2153f7e7e62ae5804a27bfcc
SHP <shp@stud.mshp.ru>
2026-06-24 18:27:13 +0300

    V7

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -13,6 +14,9 @@ public class PlayerMove : MonoBehaviour
     public GameObject Mcamera;
     public static int Score = 0;
     public TextMeshProUGUI txt;
+    AudioSource coin_zvuk;
+    public Canvas canv;
+    int a = 2;
 
     public static bool IsGrounded = false, IsJumping = false;
     float JumpHeight = 6f;
@@ -22,6 +26,7 @@ public class PlayerMove : MonoBehaviour
         rb = player.GetComponent<Rigidbody2D>();
         rb_c = Mcamera.GetComponent<Rigidbody2D>();
         Mcamera.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10f);
+        coin_zvuk = player.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -53,14 +58,26 @@ public class PlayerMove : MonoBehaviour
         {
             rb.transform.position += Vector3.right * speed * Time.deltaTime;
             rb.transform.localScale = new Vector3(0.25f, 0.25f, 1f);
+            canv.transform.localScale = new Vector3(1f, 1f, 1f);
         }
         if (Input.GetKey(KeyCode.A))
         {
             rb.transform.position += Vector3.left * speed * Time.deltaTime;
             rb.transform.localScale = new Vector3(-0.25f, 0.25f, 1f);
+            canv.transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-        
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (a > 0)
+            {
+                rb.transform.position += new Vector3(0, 3, 0);
+                a -= 1;
+                Debug.Log(a);
+            }
+        }
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             
@@ -96,11 +113,23 @@ public class PlayerMove : MonoBehaviour
         if (collision.gameObject.CompareTag("Lava"))
         {
             rb.transform.position = new Vector3(-4,-3,0);
+            a = 2;
         }
 
         if (collision.gameObject.CompareTag("Coin"))
         {
             Destroy(collision.gameObject);
+            Score++;
+            coin_zvuk.Play();
+        }
+
+        if (collision.gameObject.CompareTag("Exit"))
+        {
+            if (Score >= 11)
+            {
+                SceneManager.LoadScene(1);
+            }
+            
         }
 
     }
--- Assets/Scripts/CoinCollide.cs
+++ Assets/Scripts/CoinCollide.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class CoinCollide : MonoBehaviour
 {
-    
+    public GameObject coin;
+    AudioSource zvuk;
+
+    private void Start()
+    {
+        zvuk = coin.GetComponent<AudioSource>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("coin");
             PlayerMove.Score++;
+            zvuk.Play();
         }
     }
 }
